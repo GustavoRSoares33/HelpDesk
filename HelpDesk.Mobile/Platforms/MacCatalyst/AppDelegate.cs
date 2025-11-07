ccefe48134a81de2f909a58994a0be86a5897c9c
GuLindo33 <gugars04@gmail.com>
2025-11-06 22:06:46 -0300

    Programa 100% atualizado 2025 bomba patch

--- HelpDesk.Mobile/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HelpDesk.Mobile;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
